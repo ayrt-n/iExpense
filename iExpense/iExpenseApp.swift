cc28394bc76a451fdd581cebbadea85a63fe844a
ayrton <ayrton.parkinson1@gmail.com>
2024-07-17 07:36:00 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ayrton Parkinson on 2024/07/17.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
